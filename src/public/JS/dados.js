8c12d3872da388d31042dc4668d35cb2b1b89f45
Christian Diniz <christianarrobabol@gmail.com>
2025-06-26 17:48:49 -0300

    IntegraçãoDeFunçõesRecompensasVOL1

--- src/public/JS/dados.js
+++ src/public/JS/dados.js
@@ -1,52 +1,33 @@
+//tools
+function gebiTOTAL(x,y,z){
+    let show=document.getElementById(x).textContent
+    let show2=document.getElementById(y).textContent 
+    let show3=document.getElementById(z).textContent 
+ 
+    return +show + +show2 + +show3   //DICA INSANA: COLOCAR UM + ANTES DA VARIAVEL FORÇA ELA A SE
+                                     //TRANSFORMAR EM INT
+}
+function gebiTXT(x,y){
+    document.getElementById(x).textContent=y
+}
+
+
+//ELEMENTOS:
 const tasks=[{
-    "task1": "Tempo Decorrido",
-    "task2": "Desempenho",
-    "task3": "Tarefa Concluída",
+    "TempoDecorrido":{
+        nome: "Tempo Decorrido",
+        peso: 10
+    },
+    "Default":{
+        nome: "Tarefa Concluída",
+        peso: 1
+    },
+    "Desempenho":{
+        nome: "Desempenho",
+        peso: 50
+    }
+    
 }]
-let dados=[{
-    "t_concluida": "t_concluida",
-    "horas": "horas",
-    "segundos":"segundos",
-    "combo": "combo",
-}]
 
-let imput1=3, imput2=2,imput3=2.
-function registration(){
-localStorage.setItem(dados[0].horas, imput1)
-localStorage.setItem(dados[0].t_concluida, imput2)
-localStorage.setItem(dados[0].combo, imput3)}
 
-registration()
-store1()
-registration()
-store2()
-registration()
-store3()
-
-function store1(){ 
-        horasqnt1=localStorage.getItem("horas"),
-        comboqnt1=localStorage.getItem("combo"),
-        t_concluidaqnt1=localStorage.getItem("t_concluida");
-        localStorage.removeItem(dados[0].horas)
-        localStorage.removeItem(dados[0].t_concluida)
-        localStorage.removeItem(dados[0].combo)
-}
-
-function store2(){
-        horasqnt2=localStorage.getItem("horas"),
-        comboqnt2=localStorage.getItem("combo"),
-        t_concluidaqnt2=localStorage.getItem("t_concluida");
-        localStorage.removeItem(dados[0].horas)
-        localStorage.removeItem(dados[0].t_concluida)
-        localStorage.removeItem(dados[0].combo)
-}
-
-function store3(){
-    horasqnt3=localStorage.getItem("horas"),
-    comboqnt3=localStorage.getItem("combo"),
-    t_concluidaqnt3=localStorage.getItem("t_concluida");
-    localStorage.removeItem(dados[0].horas)
-    localStorage.removeItem(dados[0].t_concluida)
-    localStorage.removeItem(dados[0].combo)
-}
 
